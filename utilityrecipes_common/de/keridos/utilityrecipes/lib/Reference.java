496e761e704bb1b4c3941102ae4b4f8b8f505c65
Keridos <fuffzehn@gmail.com>
2013-06-01 20:55:25 +0200

    changed mcmod, removed line from reference file

--- utilityrecipes_common/de/keridos/utilityrecipes/lib/Reference.java
+++ utilityrecipes_common/de/keridos/utilityrecipes/lib/Reference.java
@@ -1,7 +1,6 @@
 package de.keridos.utilityrecipes.lib;
 
 public class Reference {
-
     public static final String MOD_ID = "UtilityRecipes";
     public static final String MOD_NAME = "Utility Recipes";
     public static final String VERSION = "0.0.1";
