c04ed0c03e76df73babc816505b614b7fa7ff72e
Keridos <fuffzehn@gmail.com>
2013-11-12 22:19:11 +0100

    gui now opens correctly

--- utilityrecipes_common/de/keridos/utilityrecipes/client/container/ContainerAutoCrafter.java
+++ utilityrecipes_common/de/keridos/utilityrecipes/client/container/ContainerAutoCrafter.java
@@ -4,6 +4,7 @@ import de.keridos.utilityrecipes.tileentity.TileEntityAutoCrafter;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
+import net.minecraft.inventory.Slot;
 
 /**
  * Created with IntelliJ IDEA.
@@ -17,10 +18,20 @@ public class ContainerAutoCrafter extends Container {
 
     public ContainerAutoCrafter(InventoryPlayer invPlayer, TileEntityAutoCrafter entity) {
         this.autoCrafter = entity;
+        for (int y = 0; y < 3; y++) {
+            for (int x = 0; x < 9; x++) {
+                this.addSlotToContainer(new Slot(invPlayer, 9 + x + y * 9, 8 + x * 18, 132 + y * 18));
+            }
+        }
+        for (int x = 0; x < 9; x++) {
+            this.addSlotToContainer(new Slot(invPlayer, x, 8 + x * 18, 190));
+        }
     }
 
     @Override
     public boolean canInteractWith(EntityPlayer player) {
         return autoCrafter.isUseableByPlayer(player);  //To change body of implemented methods use File | Settings | File Templates.
     }
+
+
 }
